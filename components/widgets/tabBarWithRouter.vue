a23e33338996536a4fc5c807d16448dfacc14e5b
unknown <JeffYang@boyaa.com>
2016-06-21 11:28:48 +0800

    update tabBarWithRouter Components

---
+++ components/widgets/tabBarWithRouter.vue
@@ -0,0 +1,37 @@
+<template>
+    <header>
+        <a href="javascript:;" id="tab-bar-back" @click="onBackToIndexClick" v-if="isShowBackBtnFlag"></a>
+        <nav>
+            <ul id="tab-bar-ul">
+                <a v-for="(index, item) in tabList" v-link="item.path" class="tab-bar-li">
+                    {{ item.name }}
+                </a>
+                <a v-link="{ path: 'main' }" class="tab-bar-li">main page tab</a>
+            </ul>
+        </nav>
+    </header>
+    <router-view></router-view>
+</template>
+
+
+<script>
+export default {
+    props: ['tabList','isShowBackBtnFlag'],
+    data () {
+        return {
+        }
+    },
+    ready(){
+        location.href = location.href + '/';
+    },
+    methods:{
+        onBackToIndexClick(){
+            var href = window.location.href;
+            var index = href.lastIndexOf("#");
+            var actRuter = href.substring(index);
+            href = href.replace(actRuter , '');
+            window.location.href = href;
+        }
+    }
+}
+</script>
---
+++ demo/config/tabbar.config.js
@@ -0,0 +1,16 @@
+module.exports = function() {
+	return {
+        "0":{
+            "index": '/main',
+            "name": "Tab Main Page",
+            "components": "./main.vue",
+            "path": "main"
+        },
+        "1":{
+            "index": '/rule',
+            "name": "Tab Rule Page",
+            "components": "./rule.vue",
+            "path": "rule"
+        }
+	}
+}
